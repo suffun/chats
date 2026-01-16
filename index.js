209f629f7a8b50a5919d47b2065809e9e52f1c2f
sufiyan khan <suffunew@gmail.com>
2026-01-16 22:21:37 +0530

    create a new route

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Chat = require("./models/chat.js");
-const path = require("path"); 
+const path = require("path");
 
-app.set("views",path.join(__dirname,"views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 
 main().then(() => {
@@ -17,12 +20,40 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 };
 // index route
-app.get("/chats",async (req,res) => {
-     let chats = await Chat.find();
-     console.log(chats);
-     res.render("index",{chats});
+app.get("/chats", async (req, res) => {
+    let chats = await Chat.find();
+
+    res.render("index", { chats });
 
 });
+
+// NEW ROUTE
+app.get("/chats/new", async (req, res) => {
+    res.render("new");
+
+});
+
+// Create route
+app.post("/chats", (req, res) => {
+    let { from, to, msg } = req.body;
+
+    let newChat = new Chat({
+        from: from,
+        to: to,
+        msg: msg,
+        created_at: new Date()
+    });
+    newChat.save()
+        .then((result) => {
+            console.log("Chat was saved");
+            res.redirect("/chats");
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send("Error occurred");
+        });
+});
+
 
 app.get("/", (req, res) => {
     res.send("root is working fine bro")
